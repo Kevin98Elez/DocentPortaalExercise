6ca425779811c1b4f04e1712103a7d86d217e0aa
Kevin98 <kevinjespers98@gmail.com>
2023-05-30 11:48:20 +0200

    usable map in welcome

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+     crossorigin=""/>
+     <!-- Make sure you put this AFTER Leaflet's CSS -->
+        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+        crossorigin=""></script>
+        <style>
+            #map {
+                width: 100%;
+                height: 400px;
+            }
+        </style>
+        <link rel="stylesheet" href="{{ asset('css/ol.css') }}" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>@yield('title')</title>
 </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,4 +4,45 @@
     {{"HomePage"}}
 @endsection
 
+
+@section('map')
+<div id="map"></div>
+    <script src="{{ asset('js/ol.js') }}"></script>
+        <script>
+            var map = new ol.Map({
+                target: 'map',
+                layers: [
+                    new ol.layer.Tile({
+                        source: new ol.source.OSM()
+                    })
+                ],
+                view: new ol.View({
+                    center: ol.proj.fromLonLat([5.536490, 50.994720]), // Genk coordinates: [longitude, latitude]
+                zoom: 10 // Adjust the zoom level as needed
+                })
+            });
+
+                // Create a marker overlay
+            var marker = new ol.Feature({
+                geometry: new ol.geom.Point(ol.proj.fromLonLat([5.536490, 50.994720])) // Marker coordinates: [longitude, latitude]
+            });
+            var markerStyle = new ol.style.Style({
+                image: new ol.style.Icon({
+                    anchor: [0.5, 1], // Set the anchor point of the marker icon
+                    src: 'https://openlayers.org/en/latest/examples/data/icon.png' // URL to the marker icon image
+                })
+            });
+            marker.setStyle(markerStyle);
+            var vectorSource = new ol.source.Vector({
+                features: [marker]
+            });
+            var vectorLayer = new ol.layer.Vector({
+                source: vectorSource
+            });
+            map.addLayer(vectorLayer);
+        </script>
+@endsection
+
 @section('content')
+<a> help</a>
+@endsection
