7f84fa4946a5f2d9eed36b1e39c2a3dd0efeff52
Kevin98 <kevinjespers98@gmail.com>
2023-05-30 11:16:09 +0200

    navigation visual changes

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,13 +1,15 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href={{route('welcome')}}>
+        <img src="https://www.syntra.be/wp-content/uploads/2022/07/Synta-logo-klein.png" jsaction="VQAsE" class="r48jcc pT0Scc iPVvYb" alt="SYNTRA" jsname="kn3ccd" data-iml="208049.60000002384" aria-hidden="true" width="70px">
+      </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href={{route('welcome')}}>Home</a>
+            <a class="nav-link active" aria-current="page" href={{route('welcome')}}>Leerkrachten</a>
           </li>
           
 
